431fd234de1ffdc31c41736f2f85a611a5b878bb
LAPTOP-3IMRTDAG\Advay <advay.gokul@gmail.com>
2022-06-01 18:51:46 +0530

    initial commit

---
+++ archive/html_Exercises/cars/Exercise 1.py
@@ -0,0 +1,3 @@
+print("Create Hammer")
+print("Create Nails")
+print("Use hammer to bang nails into a wall")
---
+++ archive/html_Exercises/cars/come on.py
@@ -0,0 +1,41 @@
+print("I'm Advay")
+input("Where am I?")
+print("Oh, I just noticed, I'm on the road!")
+input('What is this?')
+print("Oh, This is a Hammer.")
+print("What shall I do with this?")
+print("Ah, I see!")
+print("There's a bag of nails over there on the other side of the pavement.")
+print("I will use these to create a wooden block to help stop the leak from the dam.")
+input("But should I do it? It is risky.I might hurt myself")
+print("Ok.Fine.I shall do it.")
+print("Oh no! I forgot to count the number of nails I have!")
+nails_possession = "15."
+print("I have "  + nails_possession  )     
+print("Now that i have counted my nails, we can set off to the dam!")
+print("The route to the dam is called Arithmancy route.I would love to do some Math problems on the way! ")
+print("It'll be fun!")
+print("Look the first sum!")
+input("Add  3 and 2:")
+a=3
+b=2
+print("Addition: ", a+b)
+print("Ooh, a new question!")
+input("Divide 2,125 by 17: ")
+a=2125
+b=17
+print("Division (floor) :", a // b)
+a=52
+b=4
+print("Hmm, looks easy...")
+input("Multiply 52 and 4:")
+print("Multiplication : ", a*b)
+a=70
+b=34
+print("Easy enough!")
+input("Subtract 34 from 70:")
+print("Subtraction : ", a-b)
+
+
+
+
